56b8bfa1bd632360dbf1a30049891c1b9b6e7e1e
Stefan Gründel <stefan.gruendel@gmail.com>
2025-09-12 15:59:14 +0200

    add PowerShell profile and tool install script

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,89 @@
+# PowerShell 7
+
+function ls {
+    eza -lh --group-directories-first --icons=auto @Args
+}
+
+function lsa {
+    ls -a @Args
+}
+
+function lt {
+    eza --tree --level=2 --long --icons --git @Args
+}
+
+function lta {
+    lt -a @Args
+}
+
+function ff {
+    fzf --preview "bat --style=numbers --color=always {}" @Args
+}
+
+function cd {
+    param(
+        [string]$Path
+    )
+
+    if (-not $Path) {
+        Set-Location ~
+        return
+    }
+
+    if (Test-Path $Path -PathType Container) {
+        Set-Location $Path
+    }
+    else {
+        # if you use zoxide (z) in Windows, call it here
+        z $Path
+        if ($?) {
+            Write-Host "`u{f17a9} $(Get-Location)" -ForegroundColor Cyan
+        }
+        else {
+            Write-Host "Error: Directory not found" -ForegroundColor Red
+        }
+    }
+}
+
+# Open files like xdg-open
+function open {
+    param([string[]]$Paths)
+    foreach ($p in $Paths) {
+        Start-Process $p
+    }
+}
+
+# Directories, need to prefix with _ as just dots are not allowed in PowerShell
+function _.. { Set-Location .. }
+function _... { Set-Location ../.. }
+function _.... { Set-Location ../../.. }
+
+# Tools
+Set-Alias g git
+Set-Alias r rails
+Set-Alias j java
+Set-Alias d docker
+
+function n {
+    if ($Args.Count -eq 0) {
+        nvim .
+    } else {
+        nvim @Args
+    }
+}
+
+# Git
+function gcm { git commit -m "$Args" }
+function gcam { git commit -a -m "$Args" }
+function gcad { git commit -a --amend @Args }
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# -----------------------------
+# Optional: Enable predictive history and IntelliSense
+# -----------------------------
+if ($PSVersionTable.PSVersion.Major -ge 7) {
+    Import-Module PSReadLine
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+}
---
+++ install_tools.ps1
@@ -0,0 +1,10 @@
+# install PowerShell 7
+winget install --id Microsoft.Powershell
+
+# install tools
+winget install -e sharkdp.bat
+winget install -e eza-community.eza
+winget install -e sharkdp.fd
+winget install -e fzf
+winget install -e BurntSushi.ripgrep.MSVC
+winget install -e ajeetdsouza.zoxide
