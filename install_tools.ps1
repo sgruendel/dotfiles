484aece25568e8dbcd7ea8ff89fe3f6496e3b0d1
Stefan Gründel <stefan.gruendel@gmail.com>
2025-12-17 07:17:04 +0100

    install yazi

--- install_tools.ps1
+++ install_tools.ps1
@@ -5,10 +5,13 @@ winget install --id Microsoft.Powershell
 winget install -e sharkdp.bat
 winget install -e eza-community.eza
 winget install -e sharkdp.fd
-winget install -e fzf
+winget install -e junegunn.fzf
 winget install -e BurntSushi.ripgrep.MSVC
 winget install -e starship
 winget install -e ajeetdsouza.zoxide
+
+# https://yazi-rs.github.io/docs/installation/#install-with-winget
+winget install -e sxyazi.yazi jqlang.jq
 
 # install neovim / LazyVim
 winget install -e Neovim.Neovim
