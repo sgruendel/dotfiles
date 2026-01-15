765c8e2060b86ea32f96bc78c69f96c83aae558a
Stefan Gründel <stefan.gruendel@gmail.com>
2026-01-15 07:26:08 +0100

    update Neovim installation to use Nightly version

--- install_tools.ps1
+++ install_tools.ps1
@@ -14,7 +14,7 @@ winget install -e ajeetdsouza.zoxide
 winget install -e sxyazi.yazi jqlang.jq
 
 # install neovim / LazyVim
-winget install -e Neovim.Neovim
+winget install -e Neovim.Neovim.Nightly
 git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
 Remove-Item $env:LOCALAPPDATA\nvim\.git -Recurse -Force
 
