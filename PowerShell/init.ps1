c98572de6deaddd0982252343d6a9af3bb654a30
Stefan Gründel <stefan.gruendel@gmail.com>
2026-05-12 14:06:48 +0200

    feat: conditionally initialize starship in PowerShell based on host and environment

--- PowerShell/init.ps1
+++ PowerShell/init.ps1
@@ -1,6 +1,12 @@
 # see https://github.com/basecamp/omarchy/blob/master/default/bash/init
 (&mise activate pwsh) | Out-String | Invoke-Expression
 
-(&starship init powershell) | Out-String | Invoke-Expression
+if (
+	$Host.Name -in @('ConsoleHost', 'Visual Studio Code Host') -and
+	$env:TERM -ne 'dumb' -and
+	(Get-Command starship -ErrorAction SilentlyContinue)
+) {
+	(&starship init powershell) | Out-String | Invoke-Expression
+}
 
 (&zoxide init powershell) | Out-String | Invoke-Expression
