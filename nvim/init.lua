9b2b57c4d2e60c89bb3a655ebb2dc84dd0362b4a
Stefan Gründel <stefan.gruendel@gmail.com>
2026-08-23 20:04:36 +0200

    feat: set light Catppuccin Mocha window separator color

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Catppuccin Mocha Overlay 1 foreground color
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#7f849c" })
