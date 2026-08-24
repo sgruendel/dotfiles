10b8df79921094652a850bbf1f941c4f273c4283
Stefan Gründel <stefan.gruendel@gmail.com>
2026-08-24 13:15:29 +0200

    feat: use opencode --auto as in Omarchy Quattro

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.g.clipboard = {
+	name = "win32yank-wsl",
+	copy = {
+		["+"] = "win32yank.exe -i --crlf",
+		["*"] = "win32yank.exe -i --crlf",
+	},
+	paste = {
+		["+"] = "win32yank.exe -o --lf",
+		["*"] = "win32yank.exe -o --lf",
+	},
+	cache_enabled = 0,
+}
+
 -- Catppuccin Mocha Overlay 1 foreground color
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#7f849c" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,11 +1,23 @@
--- Options are automatically loaded before lazy.nvim startup.
-require("config.remote_clipboard").setup()
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
 
 -- vim.opt.relativenumber = false
 vim.g.autoformat = false
 
 vim.filetype.add({
-  extension = {
-    jte = "html",
-  },
+	extension = {
+		jte = "html",
+	},
 })
+
+-- thicker split chars
+-- vim.opt.fillchars = {
+-- 	horiz = "━",
+-- 	horizup = "┻",
+-- 	horizdown = "┳",
+-- 	vert = "┃",
+-- 	vertleft = "┫",
+-- 	vertright = "┣",
+-- 	verthoriz = "╋",
+-- }
