e2efb8ff8ee3fffb555def121118bce6f129292e
Stefan Gründel <stefan.gruendel@gmail.com>
2026-08-24 13:29:47 +0200

    Revert "feat: use opencode --auto as in Omarchy Quattro"
    This reverts commit 10b8df79921094652a850bbf1f941c4f273c4283.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,23 +1,11 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Options are automatically loaded before lazy.nvim startup.
+require("config.remote_clipboard").setup()
 
 -- vim.opt.relativenumber = false
 vim.g.autoformat = false
 
 vim.filetype.add({
-	extension = {
-		jte = "html",
-	},
+  extension = {
+    jte = "html",
+  },
 })
-
--- thicker split chars
--- vim.opt.fillchars = {
--- 	horiz = "━",
--- 	horizup = "┻",
--- 	horizdown = "┳",
--- 	vert = "┃",
--- 	vertleft = "┫",
--- 	vertright = "┣",
--- 	verthoriz = "╋",
--- }
