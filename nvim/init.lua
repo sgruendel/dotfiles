6f32da6293ec23642a400ad7db2abb4dadb76eed
Stefan Gründel <stefan.gruendel@gmail.com>
2026-08-29 10:21:50 +0200

    feat: in WSL, use win32yank.exe for NeoVim clipboard copy/paste

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,18 @@ require("config.lazy")
 
 -- Catppuccin Mocha Overlay 1 foreground color
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#7f849c" })
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
